0ae344ee8606486fcf09a03ce6f85a75a340c010
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-04-14 16:04:57 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -13,13 +13,14 @@ for month in $(seq -w 1 12); do
     file_prefix="RC_${year}-${month}"
 
     wget "https://files.pushshift.io/reddit/comments/${file_prefix}.zst"
+    echo "We are at stage 1: Conversion of a zst file to txt"
     python3 1_zst2text.py ${file_prefix}.zst
-    # Sentence parsing, train test split
+    echo "We are at stage 2: Sentence parsing, train test split"
     python3 2_traintestsplit.py ${file_prefix}.txt
-    # Noise filtering
+    echo "We are at stage 3: Noise filtering"
     python3 3_cleaning.py ${file_prefix}_train.txt
     python3 3_cleaning.py ${file_prefix}_test.txt
-    # Deduplication of sentences and language detection
+    echo "We are at stage 4: Deduplication of sentences and language detection"
     python3 4_deduplicate_language_detect.py cleaned_${file_prefix}_train.txt
     python3 4_deduplicate_language_detect.py cleaned_${file_prefix}_test.txt
 done
