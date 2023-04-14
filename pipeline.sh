f81015f06cd9af412e4255bf2d5d5b1d09285d27
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-04-14 15:56:23 +0200

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -6,14 +6,16 @@
 #SBATCH --mem=256G
 #SBATCH --time=110:00:00
 #SBATCH --partition=gpu
-module purge
 
 
 wget "https://files.pushshift.io/reddit/comments/RC_2022-01.zst"
 python3 1_zst2text.py RC_2020-01.zst
+# Sentence parsing, train test split
 python3	2_traintestsplit.py RC_2020-01.txt
+# Noise filtering
 python3	3_cleaning.py RC_2020-01_train.txt
 python3	3_cleaning.py RC_2020-01_test.txt
+# Deduplication of sentences and language detection
 python3	4_deduplicate_language_detect.py cleaned_RC_2022-01_train.txt
 python3	4_deduplicate_language_detect.py cleaned_RC_2022-01_test.txt
 
