89c6cf75ea43a6e74c89c3e5f4240bc07d0a9fc8
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-04-15 23:18:43 +0200

    Update 1_zst2text.py

--- 1_zst2text.py
+++ 1_zst2text.py
@@ -38,8 +38,10 @@ def read_lines_zst(file_name):
             buffer = lines[-1]
         reader.close()
 
-
+keywords = ['nterest rat', 'nflation', 'mploy', 'overnment', 'anking syst', 'limate chang', "kraine", 'nternational coop', ]
 if __name__ == "__main__":
+    min_account_age = 30 * 24 * 60 * 30  # 15 days in seconds
+    min_karma = 10
     file_path = sys.argv[1]
     file_size = os.stat(file_path).st_size
     file_lines = 0
@@ -47,9 +49,25 @@ if __name__ == "__main__":
     created = None
     bad_lines = 0
     bodies = []
+    data = {}
     for line, file_bytes_processed in tqdm(read_lines_zst(file_path), desc="Processing lines"):
         try:
             obj = json.loads(line)
+            #If an account is less than 14 days old, we don't want to consider it
+            if obj['author_created_utc']:
+                account_age = obj['created_utc'] - obj['author_created_utc']
+                if account_age < min_account_age:
+                    continue
+            if not any(keyword in obj["body"] for keyword in keywords):
+                continue
+
+            #if 'bot' in obj["author"] or account_age < min_account_age or obj["body"] == '[removed]':
+             #   continue
+            if obj['subreddit'] not in data.keys():
+                data[obj['subreddit']] = []
+            #print("OBJ IS: ")
+            #print(obj)
+            data[obj['subreddit']].append(obj["body"])
             bodies.append(obj["body"])
             created = datetime.utcfromtimestamp(int(obj['created_utc']))
         except (KeyError, json.JSONDecodeError) as err:
@@ -61,16 +79,28 @@ if __name__ == "__main__":
                 f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines:,} : {bad_lines:,} : {file_bytes_processed:,}:{(file_bytes_processed / file_size) * 100:.0f}%")
     current_body = ""
     adjusted_bodies = []
-    for i in bodies:
-        i = i.replace("[deleted]", "").replace("\n", "")
-        if len(current_body) + len(i) > 512 and len(current_body) > 0:
-            adjusted_bodies.append(current_body)
-            current_body = i
-        else:
-            current_body += i
-    new_name = file_path.split('.')[0] + '.txt'
+    log.info(f"Created subreddits count:{len(data.keys())} and their names {data.keys()}")
+
+    for k,v in data.items():
+        for post in v:
+            if len(current_body)+len(post)>512:
+                adjusted_bodies.append(current_body)
+                current_body = post
+            else:
+                current_body = current_body + ' '+post
+    #for i in bodies:
+        #i = i.replace("[deleted]", "").replace("\n", "")
+    #    if len(current_body) + len(i) > 512 and len(current_body) > 0:
+    #        adjusted_bodies.append(current_body)
+    #        current_body = i
+    #    else:
+    #        current_body += i
+
+
+    new_name = 'updated_'+file_path.split('.')[0] + '.txt'
     f = open(new_name, "w")
     for i in adjusted_bodies:
         f.write(i)
-        f.write('\n')
+        #f.write('\n')
     log.info(f"Complete : {file_lines:,} : {bad_lines:,}")
+
