ffe394fe262299a51eb1ad63b2cec941fdb12b6a
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-05-10 15:52:13 +0200

    Update 1_zst2text.py
    The output should have each comment on a separate line, filtering of keywords disabled.

--- 1_zst2text.py
+++ 1_zst2text.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import zstandard
 import os
 import json
@@ -5,13 +7,24 @@ import sys
 from datetime import datetime
 import logging.handlers
 from tqdm import tqdm 
+import re
 
 
 log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
 
+KEYWORD_FILTERING = False
+MAKING_BLOCKS = False
+keywords = ['nterest rat', 'nflation', 'eflation', 'mploy', 'overnment', 'anking syst', 'limate chang', "kraine", 'nternational coop' ]
+min_account_age = 30 * 24 * 60 * 30  # 15 days in seconds
+min_karma = 10 # currently not used
+
+# This script takes a .zst file as input argument and outputs a text file of comments it contains
+# hyperplarameters keywords, KEYWORD_FILTERING and MAKING_BLOCKS can be turned on/off and changed and left to one's discression
+
 def read_and_decode(reader, chunk_size, max_window_size, previous_chunk=None, bytes_read=0):
+
     chunk = reader.read(chunk_size)
     bytes_read += chunk_size
     if previous_chunk is not None:
@@ -39,19 +52,17 @@ def read_lines_zst(file_name):
             buffer = lines[-1]
         reader.close()
 
-keywords = ['nterest rat', 'nflation', 'eflation', 'mploy', 'overnment', 'anking syst', 'limate chang', "kraine", 'nternational coop' ]
+
 if __name__ == "__main__":
-    min_account_age = 30 * 24 * 60 * 30  # 15 days in seconds
-    min_karma = 10
-    file_path = sys.argv[1]
-    file_size = os.stat(file_path).st_size
+    
+    file_path = sys.argv[1] # .zst file on input
+    file_size = os.stat(file_path).st_size 
     file_lines = 0
     file_bytes_processed = 0
     created = None
     bad_lines = 0
     bodies = []
     data = {}
-    
     for line, file_bytes_processed in tqdm(read_lines_zst(file_path), desc="Processing lines"):
         try:
             obj = json.loads(line)
@@ -60,16 +71,16 @@ if __name__ == "__main__":
                 account_age = obj['created_utc'] - obj['author_created_utc']
                 if account_age < min_account_age:
                     continue
-            if not any(keyword in obj["body"] for keyword in keywords):
+            
+            if KEYWORD_FILTERING and not any(keyword in obj["body"] for keyword in keywords):
                 continue
 
-            #if 'bot' in obj["author"] or account_age < min_account_age or obj["body"] == '[removed]':
-             #   continue
+            # Just making notes of what subreddits have already been visited
             if obj['subreddit'] not in data.keys():
                 data[obj['subreddit']] = []
-            #print("OBJ IS: ")
-            #print(obj)
             data[obj['subreddit']].append(obj["body"])
+
+            # Saving the text of each comment
             bodies.append(obj["body"])
             created = datetime.utcfromtimestamp(int(obj['created_utc']))
         except (KeyError, json.JSONDecodeError) as err:
@@ -79,30 +90,32 @@ if __name__ == "__main__":
         if file_lines % 100000 == 0:
             log.info(
                 f"{created.strftime('%Y-%m-%d %H:%M:%S')} : {file_lines:,} : {bad_lines:,} : {file_bytes_processed:,}:{(file_bytes_processed / file_size) * 100:.0f}%")
+        
     current_body = ""
     adjusted_bodies = []
     log.info(f"Created subreddits count:{len(data.keys())} and their names {data.keys()}")
 
     for k,v in data.items():
         for post in v:
-            if len(current_body)+len(post)>512:
-                adjusted_bodies.append(current_body)
-                current_body = post
+            if MAKING_BLOCKS:
+                if len(current_body)+len(post)>512:
+                    adjusted_bodies.append(current_body)
+                    current_body = post
+                else:
+                    current_body = current_body + ' '+post
             else:
-                current_body = current_body + ' '+post
-    #for i in bodies:
-        #i = i.replace("[deleted]", "").replace("\n", "")
-    #    if len(current_body) + len(i) > 512 and len(current_body) > 0:
-    #        adjusted_bodies.append(current_body)
-    #        current_body = i
-    #    else:
-    #        current_body += i
+                current_body = current_body + '\n' + post
 
-
+    
+    current_body = re.sub(r"\n+", "\n", current_body)
     new_name = 'updated_'+file_path.split('.')[0] + '.txt'
+    print(current_body)
     f = open(new_name, "w")
-    for i in adjusted_bodies:
-        f.write(i)
-        #f.write('\n')
-    log.info(f"Complete : {file_lines:,} : {bad_lines:,}")
 
+    if MAKING_BLOCKS:
+        for i in adjusted_bodies:
+            f.write(i)
+    else:
+        f.write(current_body)
+
+    log.info(f"Complete : {file_lines:,} : {bad_lines:,}")
