06d047d3d57ccdcc4ac54dc2c0123780b4009c11
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-06-02 19:25:46 +0200

    Update 3_cleaning.py

--- 3_cleaning.py
+++ 3_cleaning.py
@@ -1,89 +1,75 @@
+import os
 import re
 import sys
 import time
+import logging
+from tqdm import tqdm
+
+import langid
+log = logging.getLogger("bot")
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler())
 
 input_file = sys.argv[1]
-output_file = "cleaned_" + input_file
+input_filename = os.path.basename(input_file)
+output_file = "fast_cleaned_" + input_filename
 start_time = time.time()
-block_size = 512
+
+seen_lines = set()
+non_english_lines = 0
+LANGDETECT = True # Change this to False if you want to disable language detection 
+
 with open(input_file, 'r') as f:
-    sentences = f.read()
+    total_lines = sum(1 for _ in f)
 
-# removed
-sentences = re.sub(r"\[removed\]", "", sentences)
-# links
-sentences = re.sub(r"\[link\]", "", sentences)
-# html tag
-sentences = re.sub(r"&gt;", "", sentences)
-# dash
-sentences = re.sub(r"&mdash;", "-", sentences)
-# ampersand
-sentences = re.sub(r"&amp;", "&", sentences)
-# quotation marks
-sentences = re.sub("&quot;", "\"",sentences)
-# remove HTML entities from the example string
-sentences = re.sub(r"&[#\w]+(?:;\s*|(?=\s))", "", sentences)
-# remove emojis
-sentences = re.compile("[\U00010000-\U0010ffff]", flags=re.UNICODE).sub(r"", sentences)
-# remove urls
-sentences = re.sub(r'http\S+', '', sentences)
-sentences = re.sub(r'\b\w+\.\w{2,3}(/\S*)?\b', '', sentences)
-#brackets
-sentences = re.sub(r'\[\s*\]', '', sentences)
-# asterisks and ^
-sentences = re.sub(r'[\^*]', '', sentences)
-# underscores at the start and end of words
-sentences = re.sub(r"(_+)(\w+)(_+)", r"\2", sentences)
-# markdown tags
-sentences = re.sub(r">!|!<", "", sentences)
-# hashes
-sentences = re.sub(r"#(?!\d)", "", sentences)
-# strikeout
-sentences = = re.sub(r"~{2,}", "", sentences)
-# sentences without eos
-sentences = re.sub(r'([.!?])\s+([A-Z])', r'\1 <|eos|> \2', sentences)
-# sentence ending missed by sent tokenizer
-sentences = re.sub(r"(?<=\.|\?|\!)(?=[A-Z])", " <|eos|> ", sentences)
-# sent tokeniser didn't properly end sentences detected 
-sentences = re.sub(r'([a-z]) \<\|eos\|\>', r'\1. <|eos|>', sentences)
-# sentences seeping into each other
-sentences =  re.sub(r'([0-9a-z?!])([A-Z])', r'\1. <|eos|> \2', sentences)
-# originally this included any two capital letters in succession, but were removed for ex. bcs of FTE or UAE...etc.
-sentences = re.sub(r"\b([a-z]*)\s?([A-Z][a-z]*)\b", r"\1 \2", sentences)
-# emails
-sentences = re.sub(r'\S+@\S+', '', sentences)
-# remove multiple spaces in succession
-sentences = re.sub(r'\s+', ' ', sentences)
-# prepare for splitting
-sentences = re.sub(r"\<\|eos\|\>", "<|eos|>%%34*34%%", sentences)
-my_list = list(sentences.split('%%34*34%%'))
-# adding first eos tag
-my_list[0] = "<|eos|>"+my_list[0]
-# removing trailing eos tag
-my_list.pop()
-#print(my_list)
+with open(input_file, 'r') as f, open(output_file, 'w') as out_f:
+    for line in tqdm(f, total=total_lines, desc="Processing lines"):
+        line = line.lower()  # lower case the line
 
-# Aggregate the remaining lines into new lines that are separated by one space
-# and at most 512 characters long.
-aggregated_lines = []
-current_line = ''
-for line in my_list:
-    if len(current_line) + len(line) + 1 <= block_size:
-        # Add the line to the current line with a space separator.
-        #if current_line:
-        #    current_line += ' '
-        current_line += line
-    else:
-        # Start a new line if the current line would be too long.
-        aggregated_lines.append(current_line)
-        current_line = '<|eos|>'+line
+        replacements = [
+            (r"\[removed\]|\[link\]", ""),
+            (r"&gt;|&mdash;|&amp;|&quot;", ""),
+            (r"&[#\w]+(?:;\s*|(?=\s))|[#*^_~]|>!|!<", ""),
+            (r"\[\s*\]|http\S+|\b\w+\.\w{2,3}(/\S*)?\b|\S+@\S+", ""),
+            (r"(_+)(\w+)(_+)", r"\2"),
+            (r'\s+', ' ')
+        ]
+        for pattern, replacement in replacements:
+            line = re.sub(pattern, replacement, line)
 
-# Add the last line to the list of aggregated lines.
-if current_line:
-    aggregated_lines.append(current_line)
+        line = re.compile("[\U00010000-\U0010ffff]", flags=re.UNICODE).sub(r"", line)
+        line = line.strip()
+        
+        # add a period if the line does not end with ., !, ?, :, or ;
+        if line and line[-1] not in {'.', '!', '?', ':', ';'}:
+            line = line + '.'
 
-with open(output_file, 'w') as f:
-  for line in aggregated_lines:
-    f.write(line + '\n')
+        if LANGDETECT:
+            try:
+                # Check if the line is in English
+                lang, _ = langid.classify(line)
+                if lang == 'en':
+                    # Deduplicate lines
+                    if line not in seen_lines:
+                        seen_lines.add(line)
+                        out_f.write(line + '\n')
+                else:
+                    non_english_lines += 1
+            except Exception as e:
+                log.info(f"Can't detect language for the line: {line}. Error: {str(e)}")
+
+        else:
+            if line not in seen_lines:
+                seen_lines.add(line)
+                out_f.write(line + '\n')
+
+        if (len(seen_lines) + 1) % 10000 == 0:
+            log.info(f"Processed {len(seen_lines):,} unique lines.")
+            log.info(f"Skipped {non_english_lines} non-English lines.")
+            log.info(f"Percentage of non-English lines: {non_english_lines / (len(seen_lines) + non_english_lines):.2%}")
+
 elapsed_time = time.time() - start_time
-print(f"Processed file saved as {output_file} in {elapsed_time} seconds.")
+log.info(f"Processed file saved as {output_file} in {elapsed_time} seconds.")
+log.info(f"Total non-English lines: {non_english_lines}")
+log.info(f"Percentage of non-English lines: {non_english_lines / (len(seen_lines) + non_english_lines):.2%}")
+
