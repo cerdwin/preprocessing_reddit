447c04099b0fa970dbe145f6b29bc5b6e758251d
cerdwin <47043669+cerdwin@users.noreply.github.com>
2023-06-24 19:50:14 +0200

    Update 2_cleaning.py
    Fasttext library, which performs faster than langid (https://towardsdatascience.com/benchmarking-language-detection-for-nlp-8250ea8b67c, + manual testing)

--- 2_cleaning.py
+++ 2_cleaning.py
@@ -2,30 +2,32 @@ import os
 import re
 import sys
 import time
+import fasttext
 import logging
 from tqdm import tqdm
 
-import langid
 log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
 
 input_file = sys.argv[1]
 input_filename = os.path.basename(input_file)
-output_file = "fast_cleaned_" + input_filename
+output_file = "fasttext_cleaned_" + input_filename
 start_time = time.time()
 
 seen_lines = set()
 non_english_lines = 0
-LANGDETECT = True # Change this to False if you want to disable language detection 
+LANGDETECT = True # Change this to True if you want to enable language detection
 
+# Load FastText language identification model
+model = fasttext.load_model('lid.176.bin')
+
+# Calculating the size of the input text
 with open(input_file, 'r') as f:
     total_lines = sum(1 for _ in f)
 
 with open(input_file, 'r') as f, open(output_file, 'w') as out_f:
     for line in tqdm(f, total=total_lines, desc="Processing lines"):
-        line = line.lower()  # lower case the line
-
         replacements = [
             (r"\[removed\]|\[link\]", ""),
             (r"&gt;|&mdash;|&amp;|&quot;", ""),
@@ -38,18 +40,17 @@ with open(input_file, 'r') as f, open(output_file, 'w') as out_f:
             line = re.sub(pattern, replacement, line)
 
         line = re.compile("[\U00010000-\U0010ffff]", flags=re.UNICODE).sub(r"", line)
+
         line = line.strip()
         
-        # add a period if the line does not end with ., !, ?, :, or ;
         if line and line[-1] not in {'.', '!', '?', ':', ';'}:
             line = line + '.'
 
         if LANGDETECT:
             try:
                 # Check if the line is in English
-                lang, _ = langid.classify(line)
-                if lang == 'en':
-                    # Deduplicate lines
+                predictions = model.predict(line)[0]
+                if predictions[0] == '__label__en':
                     if line not in seen_lines:
                         seen_lines.add(line)
                         out_f.write(line + '\n')
@@ -63,7 +64,7 @@ with open(input_file, 'r') as f, open(output_file, 'w') as out_f:
                 seen_lines.add(line)
                 out_f.write(line + '\n')
 
-        if (len(seen_lines) + 1) % 10000 == 0:
+        if (len(seen_lines) + 1) % 1000000 == 0:
             log.info(f"Processed {len(seen_lines):,} unique lines.")
             log.info(f"Skipped {non_english_lines} non-English lines.")
             log.info(f"Percentage of non-English lines: {non_english_lines / (len(seen_lines) + non_english_lines):.2%}")
@@ -72,4 +73,3 @@ elapsed_time = time.time() - start_time
 log.info(f"Processed file saved as {output_file} in {elapsed_time} seconds.")
 log.info(f"Total non-English lines: {non_english_lines}")
 log.info(f"Percentage of non-English lines: {non_english_lines / (len(seen_lines) + non_english_lines):.2%}")
-
